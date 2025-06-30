0b3ed0cab1cc3ea84f1eb75588863654569507c1
JoseRomualdo <jromualdo3@hotmail.com>
2025-06-30 15:13:34 -0300

    feat: add UI components including Input, Separator, Sheet, Sidebar, Skeleton, Table, and Tooltip

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import Layout from "@/components/layout";
+
+export default function Page() {
+  return (
+    <Layout>
+      <h1>Dashboard</h1>
+    </Layout>
+  )
+}
---
+++ components/app-sidebar.tsx
@@ -0,0 +1,100 @@
+"use client"
+
+import * as React from "react"
+import {
+  Bot,
+  Command,
+  FolderOpenDot
+} from "lucide-react"
+
+import { NavMain } from "@/components/nav-main"
+// import { NavProjects } from "@/components/nav-projects"
+// import { NavSecondary } from "@/components/nav-secondary"
+import { NavUser } from "@/components/nav-user"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+const data = {
+  user: {
+    name: "Junior",
+    email: "jromualdo3@hotmail.com",
+    avatar: "teste.jpg",
+  },
+  navMain: [
+    {
+      title: "Projects",
+      url: "#",
+      icon: FolderOpenDot,
+      isActive: true,
+      items: [
+        {
+          title: "List",
+          url: "/tasks/list",
+        },
+        {
+          title: "Starred",
+          url: "#",
+        },
+        {
+          title: "Settings",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Models",
+      url: "#",
+      icon: Bot,
+      items: [
+        {
+          title: "Genesis",
+          url: "#",
+        }
+      ],
+    }
+  ],
+  navSecondary: [
+
+  ],
+  projects: [
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar variant="inset" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                  <Command className="size-4" />
+                </div>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-medium">Project Management</span>
+                  <span className="truncate text-xs">GPS</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        {/* <NavProjects projects={data.projects} />
+        <NavSecondary items={data.navSecondary} className="mt-auto" /> */}
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
